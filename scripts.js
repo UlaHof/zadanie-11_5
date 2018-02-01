68bdd0122cc5e5700e6ddaca28ae8492e72b4967
Ula <chcetylkoobejrzecfilm@gmail.com>
2018-02-01 23:57:31 +0100

    add js and html file

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function Button(text) {
+    this.text = text || "Hello";
+}
+Button.prototype.create = function() {
+    var self = this;
+    this.$element = $('<button>');
+    this.$element.text(this.text);
+    this.$element.click(function() {
+        alert(this.text);
+    });
+    $('body').append(this.$element);
+}
+
+var btn1 = new Button('Hello!');
+
+btn1.create();
+
+var btn2 = new Button();
+
+btn2.create();
+
+var btn3 = new Button('sth');
+
+btn3.create();
